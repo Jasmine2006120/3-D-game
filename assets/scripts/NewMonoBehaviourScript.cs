c88b71cef3180d3d4c67e64a5f9df7410deb4954
Jasmine Abhol <abholjasmine@gmail.com>
2026-06-09 13:11:21 +0530

    Add files via upload

---
+++ assets/scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PrintInstructions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move using arrow keys");
+        Debug.Log("Don't bump into the objects");
+
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal")* Time.deltaTime*moveSpeed;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical")* Time.deltaTime*moveSpeed;
+        transform.Translate(xValue,yValue,zValue);
+    }
+}
